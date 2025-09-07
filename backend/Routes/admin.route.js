96c10ff025e87a3f5c2a3af08235b4a32b63d4b9
Atharva Patil <anpatil.1223@gmail.com>
2025-09-07 15:26:24 +0530

    added scripts and nodemon

--- backend/Routes/admin.route.js
+++ backend/Routes/admin.route.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { adminModel } from "../db.js";
+import z from "zod";
 
 const adminRouter = Router()
 
 adminRouter.post("/signup",(req,res)=>{
+
+    const email = req.body.email
+    const password = req.body.password
+    const fullName = req.body.fullName
+    const lastName = req.body.lastName
+
     
+
 })
 
 adminRouter.post("/course",(req,res)=>{
